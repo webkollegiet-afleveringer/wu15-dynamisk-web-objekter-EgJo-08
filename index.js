4db8d80e7b93128ecaec2f3e0d460886e3d5209e
EgJo-08 <U2535495@rts-365.dk>
2026-02-03 09:26:33 +0100

    har lavet opgave et har lavet det med begge console log metoder

---
+++ index.js
@@ -0,0 +1,11 @@
+let person = {
+    firstName: "John",
+    Lastname: "Johnson",
+    age: 20,
+    car: "mercedes"
+}
+
+console.log(person.firstName)
+console.log(person.Lastname)
+console.log(person["age"])
+console.log(person["car"])
