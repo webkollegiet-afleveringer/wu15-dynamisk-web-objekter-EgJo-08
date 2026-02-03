0555a813115ed475362a1091c8c17a167d81e40a
EgJo-08 <U2535495@rts-365.dk>
2026-02-03 09:37:07 +0100

    har lavet opgave 2 har console loggede med en for each by mu array og console loggede mine nestede objekter

--- index.js
+++ index.js
@@ -8,4 +8,27 @@ let person = {
 console.log(person.firstName)
 console.log(person.Lastname)
 console.log(person["age"])
-console.log(person["car"])
+console.log()
+
+
+let person2 = {
+    firstName: "John",
+    Lastname: "Johnson",
+    age: 20,
+    car: [
+        "mercedes",
+        "audi",
+        "ferrari"
+    ],
+    married: false,
+    pet: {
+        type: "cat",
+        name: "john jr"
+    }
+}
+
+person2.car.forEach(function(cars){
+    console.log(cars)
+})
+console.log(person2.pet.type)
+console.log(person2.pet.name)
